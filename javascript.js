2f028105764af30e712be510a5026397549d3893
Drew Bowling <andrewwbowling@gmail.com>
2023-02-28 22:26:34 -0700

    add the notes file and cleaned up some spacing errors in the javascript file

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var name = "andrew"
+var dingus = "brendan"
+
+var cat = "ember"
+
+cat = "charmmy"
+dingus = "Hayley"
+
+console.log (cat)
+
+console.log (dingus)
+
+- 
---
+++ notes.md
@@ -0,0 +1,3 @@
+# Notes for me
+
+- dont forget the node command in the terminal. The command looks like 'node filename.js' 
